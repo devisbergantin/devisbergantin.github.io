39a89dbacaed090bc4032c08dc97be293fbbd19d
Devis Bergantin <devisbergantin@gmail.com>
2026-02-16 20:52:04 +0100

    Update glitch.js

--- theentomologistsbreath/glitch.js
+++ theentomologistsbreath/glitch.js
@@ -266,9 +266,10 @@ if(alpha > 0 && images[currentImg]){
 
         // sezioni rotanti (intatte)
         drawStructuralSections(images[sectionImg], alpha);
+   }
 
-        frame = (frame + 1) % TOTAL_FRAMES;
-
+    frame = (frame + 1) % TOTAL_FRAMES;
     requestAnimationFrame(draw);
+}
 
-    });
+});                        
